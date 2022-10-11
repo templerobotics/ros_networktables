8df13f1e89b4410e0d16fd63e9732b385c469729
David Fiel <github@dfiel.com>
2022-10-11 16:55:45 -0400

    Use dataclass and service responses

--- src/nt_bridge.py
+++ src/nt_bridge.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 import rospy
 from ros_networktables.msg import NTEntryUpdate, TXArrayMessage, TXMessage
-from ros_networktables.srv import GetNTEntry, GetNTEntryArray, SetNTEntry, SetNTEntryArray
+from ros_networktables.srv import *
 from networktables import NetworkTables, NetworkTable
+from dataclasses import dataclass
 
 class NTbridge(object):
 
+    @dataclass(init=False)
     class TableEntry(object):
-        def __init__(self) -> None:
-            self.networktable: NetworkTable
-            self.rx_publisher: rospy.Publisher
-            self.tx_subscriber: rospy.Subscriber
-            self.tx_array_subscriber: rospy.Subscriber
+        networktable: NetworkTable
+        rx_publisher: rospy.Publisher
+        tx_subscriber: rospy.Subscriber
+        tx_array_subscriber: rospy.Subscriber
+            
 
-    def __init__(self):
+    def __init__(self) -> None:
         NetworkTables.initialize(server=rospy.get_param('~server', '10.12.34.2')) # VMX-pi IP Address
-        self._tables: dict[str, self.TableEntry] = {}
+        self._tables: dict[str, type[self.TableEntry]] = {}
 
         rx_tables: set[str] = set(rospy.get_param('~tables/rx', []))
         tx_tables: set[str] = set(rospy.get_param('~tables/tx', []))
@@ -36,31 +38,31 @@ class NTbridge(object):
         self._set_entry_service = rospy.Service('~set_entry', SetNTEntry, self._set_entry_string)
         self._set_entry_arr_service = rospy.Service('~set_entry_array', SetNTEntryArray, self._set_entry_string_array)
 
-    def _table_update(self, table, key, value, isNew):
+    def _table_update(self, table, key, value, isNew) -> None:
         ftable = str(table).split('/')[1]
         # rospy.loginfo(f"Table Update Triggered. Table: {ftable} Key: {key} Value: {value} IsNew: {isNew}")
         entry = NTEntryUpdate(table=ftable, key=key, type=str(type(value)).split("'")[1], value=str(value), time=rospy.get_rostime())
         self._tables[ftable].rx_publisher.publish(entry)
     
-    def _table_tx(self, data, table: str):
+    def _table_tx(self, data, table: str) -> None:
         # rospy.loginfo(f"Table TX {table}: {data}")
         self._tables[table].networktable.getEntry(data.key).setString(str(data.value))
 
-    def _table_tx_array(self, data, table: str):
+    def _table_tx_array(self, data, table: str) -> None:
         # rospy.loginfo(f"Table Array TX {table}: {data}")
         self._tables[table].networktable.getEntry(data.key).setStringArray(data.value)
 
-    def _get_entry_string(self, req: GetNTEntry):
-        return self._tables[req.table].networktable.getEntry(req.key).getString(None)
+    def _get_entry_string(self, req: GetNTEntryRequest) -> GetNTEntryResponse:
+        return GetNTEntryResponse(self._tables[req.table].networktable.getEntry(req.key).getString(None))
 
-    def _get_entry_string_array(self, req: GetNTEntryArray):
-        return self._tables[req.table].networktable.getEntry(req.key).getStringArray(None)
+    def _get_entry_string_array(self, req: GetNTEntryArrayRequest) -> GetNTEntryArrayResponse:
+        return GetNTEntryArrayResponse(self._tables[req.table].networktable.getEntry(req.key).getStringArray(None))
 
-    def _set_entry_string(self, req: SetNTEntry):
-        return self._tables[req.table].networktable.getEntry(req.key).setString(req.value)
+    def _set_entry_string(self, req: SetNTEntryRequest) -> SetNTEntryResponse:
+        return SetNTEntryResponse(self._tables[req.table].networktable.getEntry(req.key).setString(req.value))
 
-    def _set_entry_string_array(self, req: SetNTEntryArray):
-        return self._tables[req.table].networktable.getEntry(req.key).setStringArray(req.value)
+    def _set_entry_string_array(self, req: SetNTEntryArrayRequest) -> SetNTEntryArrayResponse:
+        return SetNTEntryArrayResponse(self._tables[req.table].networktable.getEntry(req.key).setStringArray(req.value))
 
 
 if __name__ == '__main__':
