c7e0490761f07befd4239c619f0aee420b2b230b
David Fiel <github@dfiel.com>
2022-10-11 14:44:02 -0400

    Make table dict of objects instead of nested dicts

--- src/nt_bridge.py
+++ src/nt_bridge.py
@@ -2,25 +2,33 @@
 import rospy
 from ros_networktables.msg import NTEntryUpdate, TXArrayMessage, TXMessage
 from ros_networktables.srv import GetNTEntry, GetNTEntryArray, SetNTEntry, SetNTEntryArray
-from networktables import NetworkTables
-
+from networktables import NetworkTables, NetworkTable
 
 class NTbridge(object):
+
+    class TableEntry(object):
+        def __init__(self) -> None:
+            self.networktable: NetworkTable
+            self.rx_publisher: rospy.Publisher
+            self.tx_subscriber: rospy.Subscriber
+            self.tx_array_subscriber: rospy.Subscriber
+
     def __init__(self):
         NetworkTables.initialize(server=rospy.get_param('server', '10.12.34.2')) # VMX-pi IP Address
-        self._tables = {}
+        self._tables: dict[str, self.TableEntry] = {}
 
-        rx_tables: list[str] = rospy.get_param('tables/rx', [])
-        tx_tables: list[str] = rospy.get_param('tables/tx', [])
+        rx_tables: set[str] = set(rospy.get_param('tables/rx', []))
+        tx_tables: set[str] = set(rospy.get_param('tables/tx', []))
 
-        for table in rx_tables + tx_tables:
-            entry = {'networktable': NetworkTables.getTable(table)}
+        for table in rx_tables.union(tx_tables): # Ensures table only loops once if it exists in both sets
+            entry = self.TableEntry()
+            entry.networktable = NetworkTables.getTable(table)
             if table in rx_tables:
-                entry['rx_publisher'] = rospy.Publisher(f'{table}/rx', NTEntryUpdate, queue_size=10)
-                entry['networktable'].addEntryListener(self._table_update, localNotify=rospy.get_param('/nt_bridge/localnotify', False))
+                entry.rx_publisher = rospy.Publisher(f'{table}/rx', NTEntryUpdate, queue_size=10)
+                entry.networktable.addEntryListener(self._table_update, localNotify=rospy.get_param('/nt_bridge/localnotify', False))
             if table in tx_tables:
-                entry['tx_subscriber'] = rospy.Subscriber(f'{table}/tx', TXMessage, self._table_tx, callback_args=table)
-                entry['tx_array_subscriber'] = rospy.Subscriber(f'{table}/tx_array', TXArrayMessage, self._table_tx_array, callback_args=table) 
+                entry.tx_subscriber = rospy.Subscriber(f'{table}/tx', TXMessage, self._table_tx, callback_args=table)
+                entry.tx_array_subscriber = rospy.Subscriber(f'{table}/tx_array', TXArrayMessage, self._table_tx_array, callback_args=table) 
             self._tables[table] = entry
         
         self._get_entry_service = rospy.Service('get_entry', GetNTEntry, self._get_entry_string)
@@ -32,27 +40,27 @@ class NTbridge(object):
         ftable = str(table).split('/')[1]
         # rospy.loginfo(f"Table Update Triggered. Table: {ftable} Key: {key} Value: {value} IsNew: {isNew}")
         entry = NTEntryUpdate(table=ftable, key=key, type=str(type(value)).split("'")[1], value=str(value), time=rospy.get_rostime())
-        self._tables[ftable]['rx_publisher'].publish(entry)
+        self._tables[ftable].rx_publisher.publish(entry)
     
     def _table_tx(self, data, table: str):
         # rospy.loginfo(f"Table TX {table}: {data}")
-        self._tables[table]['networktable'].getEntry(data.key).setString(str(data.value))
+        self._tables[table].networktable.getEntry(data.key).setString(str(data.value))
 
     def _table_tx_array(self, data, table: str):
         # rospy.loginfo(f"Table Array TX {table}: {data}")
-        self._tables[table]['networktable'].getEntry(data.key).setStringArray(data.value)
+        self._tables[table].networktable.getEntry(data.key).setStringArray(data.value)
 
-    def _get_entry_string(self, req):
-        return self._tables[req.table]['networktable'].getEntry(req.key).getString(None)
+    def _get_entry_string(self, req: GetNTEntry):
+        return self._tables[req.table].networktable.getEntry(req.key).getString(None)
 
-    def _get_entry_string_array(self, req):
-        return self._tables[req.table]['networktable'].getEntry(req.key).getStringArray(None)
+    def _get_entry_string_array(self, req: GetNTEntryArray):
+        return self._tables[req.table].networktable.getEntry(req.key).getStringArray(None)
 
-    def _set_entry_string(self, req):
-        return self._tables[req.table]['networktable'].getEntry(req.key).setString(req.value)
+    def _set_entry_string(self, req: SetNTEntry):
+        return self._tables[req.table].networktable.getEntry(req.key).setString(req.value)
 
-    def _set_entry_string_array(self, req):
-        return self._tables[req.table]['networktable'].getEntry(req.key).setStringArray(req.value)
+    def _set_entry_string_array(self, req: SetNTEntryArray):
+        return self._tables[req.table].networktable.getEntry(req.key).setStringArray(req.value)
 
 
 if __name__ == '__main__':
